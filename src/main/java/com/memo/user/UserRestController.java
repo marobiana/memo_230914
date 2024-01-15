f8e7c10d351948b8df74136b95c74f9e7c555534
DESKTOP-B0NSHI7\user <test@test.com>
2024-01-15 16:51:29 +0900

    아이디 중복확인, 회원가입 로직 일부

--- src/main/java/com/memo/user/UserRestController.java
+++ src/main/java/com/memo/user/UserRestController.java
@@ -3,23 +3,66 @@ package com.memo.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.memo.common.EncryptUtils;
+import com.memo.user.bo.UserBO;
+import com.memo.user.entity.UserEntity;
+
 @RequestMapping("/user")
 @RestController
 public class UserRestController {
+	
+	@Autowired
+	private UserBO userBO;
 
+	/**
+	 * 아이디 중복확인 API
+	 * @param loginId
+	 * @return
+	 */
 	@RequestMapping("/is-duplicated-id")
 	public Map<String, Object> isDuplicatedId(
 			@RequestParam("loginId") String loginId) {
 		
 		// DB 조회 - select
+		UserEntity user = userBO.getUserEntityByLoginId(loginId);
 		
 		Map<String, Object> result = new HashMap<>();
+		if (user != null) { // 중복
+			result.put("code", 200);
+			result.put("is_duplicated_id", true);
+		} else { // 중복 아님
+			result.put("code", 200);
+			result.put("is_duplicated_id", false);
+		}
+		return result;
+	}
+	
+	@PostMapping("/sign-up")
+	public Map<String, Object> signUp(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email) {
+		
+		// md5 알고리즘 => password hashing
+		// aaaa => 74b8733745420d4d33f80c4663dc5e5 
+		// aaaa => 74b8733745420d4d33f80c4663dc5e5
+		String hashedPassword = EncryptUtils.md5(password);
+		
+		// db insert
+		userBO.
+		
+		// 응답값
+		Map<String, Object> result = new HashMap<>();
 		result.put("code", 200);
-		result.put("is_duplicated_id", true);
+		result.put("result", "성공");
+		
 		return result;
 	}
 	
---
+++ src/main/java/com/memo/user/bo/UserBO.java
@@ -0,0 +1,26 @@
+package com.memo.user.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.memo.user.entity.UserEntity;
+import com.memo.user.repository.UserRepository;
+
+@Service
+public class UserBO {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	// input: loginId    output: UserEntity(있거나 or null)
+	public UserEntity getUserEntityByLoginId(String loginId) {
+		return userRepository.findByLoginId(loginId);
+	}
+	
+	// input: 파라미터 4개    output: Integer id(pk) 
+}
+
+
+
+
+
