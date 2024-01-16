9146644d2e58ee590891dae67b286ff84d6e930e
DESKTOP-B0NSHI7\user <test@test.com>
2024-01-16 18:43:47 +0900

    글쓰기 마크업

--- src/main/java/com/memo/post/PostController.java
+++ src/main/java/com/memo/post/PostController.java
@@ -35,4 +35,19 @@ public class PostController {
 		model.addAttribute("viewName", "post/postList");
 		return "template/layout";
 	}
+	
+	/**
+	 * 글쓰기 화면
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/post-create-view")
+	public String postCreateView(Model model) {
+		model.addAttribute("viewName", "post/postCreate");
+		return "template/layout";
+	}
 }
+
+
+
+
