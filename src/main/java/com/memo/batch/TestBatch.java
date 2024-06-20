552de9bdd96b350148b817fbe49ee261ad460868
Take Action <69413030+marobiana@users.noreply.github.com>
2024-06-20 11:40:11 +0900

    Update TestBatch.java

--- src/main/java/com/memo/batch/TestBatch.java
+++ src/main/java/com/memo/batch/TestBatch.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class TestBatch {
-	@Scheduled(cron = "0 */1 * * * *")
+	//@Scheduled(cron = "0 */1 * * * *")
 	public void task() {
 		log.info("!!####### batch 수행");
 	}
